3f44ccd015ba54213cefde68dc44c39ac6d78c06
Abu Pras <33302808+agilbudi@users.noreply.github.com>
2026-03-06 18:25:47 +0000

    fix: build target

--- my-app/src/master.tsx
+++ my-app/src/master.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export default function App() {
-  const [inisial, setInisial] = useState('');
+  const [inisial] = useState('');
 
 
   return (
