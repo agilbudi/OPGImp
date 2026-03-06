aa32f0434a1fac71e98adbea8fc34f3f6e9ec1a1
Abu Pras <33302808+agilbudi@users.noreply.github.com>
2026-03-06 19:37:14 +0000

    fix: -

--- my-app/src/master.tsx
+++ my-app/src/master.tsx
@@ -1,20 +1,19 @@
-import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export default function App() {
-  const [inisial] = useState('');
 
 
   return (
+    <>
     <Container>
       <Row className="justify-content-center mt-5">
         <Col md={6}>
             <h1 className="mb-4">OPG App</h1>
-            <h4 className="mb-4">Inisial: {inisial}</h4>
         </Col>
       </Row>
     </Container>
+    </>
   );
 }
